b2f23caa51bd2307d369bd2219bbed9198a062ff
Назар Остафійчук <ostafii4uk@gmail.com>
2024-03-15 11:16:58 +0200

    Added PostsItem and PostsGrid components

--- components/home-page/featured-post.jsx
+++ components/home-page/featured-post.jsx
@@ -1,3 +1,11 @@
-function FeaturedPost() {}
+import classes from './featured-posts.module.css'
+
+function FeaturedPost() {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+    </section>
+  )
+}
 
 export default FeaturedPost
