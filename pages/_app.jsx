e862061a38cb83160ec390a6cf702ba3af5c5cb8
Назар Остафійчук <ostafii4uk@gmail.com>
2024-03-20 10:38:57 +0200

    Added next.config.js file into app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>My blog</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
