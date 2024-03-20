dd59c62020b141765457bbd8e4d0c803ad3844eb
Назар Остафійчук <ostafii4uk@gmail.com>
2024-03-20 10:44:58 +0200

    Reducing code size

--- components/posts/post-detail/post-content.jsx
+++ components/posts/post-detail/post-content.jsx
@@ -1,9 +1,12 @@
 import ReactMarkdown from 'react-markdown'
 import Image from 'next/image'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark'
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
 import PostHeader from './post-header'
 import classes from './post-content.module.css'
+
+SyntaxHighlighter.registerLanguage('js', js)
 
 function PostContent(props) {
   const { post } = props
